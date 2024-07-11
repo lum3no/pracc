cd6cbd9e10f85d90a96faced7800872a1ca932ef
lum3no <fima.afisha@mail.ru>
2024-07-11 23:59:15 +0300

    полностью готовая программа

--- forcommits/for commits/hard8.cpp
+++ forcommits/for commits/hard8.cpp
@@ -46,7 +46,16 @@ public:
         return false;
     }
 
-    
+    vector<double> getOperators(double a, double b) {
+        vector<double> results;
+        results.push_back(a + b);
+        results.push_back(a - b);
+        results.push_back(b - a);
+        results.push_back(a * b);
+        if (b != 0) results.push_back(a / b);
+        if (a != 0) results.push_back(b / a);
+        return results;
+    }
 };
 
 int main() {
@@ -70,4 +79,3 @@ int main() {
     }
 
     return 0;
-}
